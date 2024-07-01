407835721f9a8c6e0adb32ff6fe88d07fd4fa18a
Conrad <conrad.roeber@mailbox.org>
2024-07-01 10:45:18 +0200

    Slight adaptations to work with the notebook.

--- wayback_google_analytics/output.py
+++ wayback_google_analytics/output.py
@@ -2,6 +2,10 @@ from datetime import datetime
 import json
 import os
 import pandas as pd
+import graphviz
+import re
+
+from wayback_google_analytics.codes import main_UA_code
 
 
 def init_output(type, output_dir="./output"):
@@ -15,10 +19,10 @@ def init_output(type, output_dir="./output"):
         None
     """
 
-    valid_types = ["csv", "txt", "json", "xlsx"]
+    valid_types = ["csv", "txt", "json", "xlsx", "dot"]
     if type not in valid_types:
         raise ValueError(
-            f"Invalid output type: {type}. Please use csv, txt, xlsx or json."
+            f"Invalid output type: {type}. Please use csv, txt, xlsx, json, dot."
         )
 
     # Create output directory if it doesn't exist
@@ -62,6 +66,11 @@ def write_output(output_file, output_type, results):
         with open(output_file, "w") as f:
             json.dump(results, f, indent=4)
         return
+
+    elif output_type == "dot":
+        output_dot(output_file, results)
+        return
+
     # If csv or xlsx, convert results to pandas dataframes.
     urls_df = get_urls_df(results)
     codes_df = get_codes_df(results)
@@ -211,3 +220,66 @@ def format_active(list):
     """
 
     return "\n\n".join(f"{i + 1}. {item}" for i, item in enumerate(list))
+
+
+def output_dot(filename, data):
+    """Take the data and output it in PNG and SVG formats.
+
+    @param filename: Name of the (temporary) file from which the graphic files will be created
+    @param data: Results from the scraper.
+
+    If the code is running in a Google Colab notebook then the PNG file is also displayed.
+    """
+    g = graphviz.Digraph("G")
+    g.graph_attr['rankdir'] = 'LR'
+    g.graph_attr['fontname'] = 'Arial'
+    nodes = {}
+    edges = {}
+
+    def write_node(nodes, label, shape='ellipse'):
+      if not label in nodes:
+        nodes.append(label)
+        g.node(label, shape=shape)
+
+    def write_edge(edges, source, target):
+        if not (source, target) in edges:
+            g.edge(source, target)
+            edges.append((source, target))
+        else:
+            print("Edge already contained:", source, target)
+
+    nodes = []
+    edges = []
+
+    for result in data:
+        for url in result:
+            write_node(nodes, url, shape='box')
+            if "current_UA_code" in result[url]:
+                UA_code = result[url]["current_UA_code"]
+                write_node(nodes, UA_code)
+                write_edge(edges, url, UA_code)
+                write_node(nodes, main_UA_code(UA_code))
+                write_edge(edges, main_UA_code(UA_code), url)
+            if "archived_UA_codes" in result[url]:
+                archived_UA_codes = result[url]["archived_UA_codes"]
+                # print(archived_UA_codes)
+                for UA_code in archived_UA_codes:
+                    first_seen = archived_UA_codes[UA_code]['first_seen']
+                    first_seen = re.sub(':', '', first_seen)
+                    last_seen = archived_UA_codes[UA_code]['last_seen']
+                    last_seen = re.sub(':', '', last_seen)
+                    write_node(nodes, main_UA_code(UA_code))
+                    write_edge(edges, main_UA_code(UA_code), url)
+                    write_node(nodes, f'{UA_code}\\n{first_seen}\\n{last_seen}')
+                    write_edge(edges, url, f'{UA_code}\\n{first_seen}\\n{last_seen}')
+            if "current_GA_code" in result[url]:
+                GA_code = result[url]["current_GA_code"]
+                write_node(nodes, GA_code)
+                write_edge(edges, url, GA_code)
+            if "current_GTM_code" in result[url]:
+                GTM_code = result[url]["current_GTM_code"]
+                write_node(nodes, GTM_code)
+                write_edge(edges, url, GTM_code)
+
+    g.render(engine='dot', outfile=re.sub('.dot$', '.svg', filename))
+    g.render(engine='dot', outfile=re.sub('.dot$', '.png', filename))
